39efe62ffade5086373a3ea3ec1cd677849a3001
Jay Yu <ygh970627@gmail.com>
2024-10-10 12:30:19 -0500

    Add : Adding simplifying commands to the package.json

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { signOut } from '../login/actions';
+import { createClient } from '@/utils/supabase/client';
 
-export default function Page() {
+export default async function Page() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  console.log(user);
   return (
     <Button
       onClick={() => {
