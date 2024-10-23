4a483372177f2d709ce47a8843125af150ac221a
jneduelan <jneduelan@icloud.com>
2024-10-23 10:23:06 -0500

    updated front-end and applied global layout structure

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export default function Layout({ children }) {
+  return (
+    <div className="container">
+      <header>
+        <nav>
+          <h1>Sonify for Graphs</h1>
+          <ul>
+            <li>
+              {/* link without <a> */}
+              <Link href="/login">Sign In</Link>
+            </li>
+            <li>
+              <Link href="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <p>© 2024 Sonify for Graphs Project</p>
+      </footer>
+    </div>
+  );
+}
