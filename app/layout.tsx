ff634ea6de882b79497613d3a11e32675898583d
Navin Chikkodi <73332645+og-nav@users.noreply.github.com>
2024-10-24 12:59:38 -0500

    Revert "added template shadcn/ui sidebar"
    This reverts commit 5ec7f2825a2b9a81f949845a73afc6160dc186e5.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,7 @@ export default function RootLayout({
         <title>{metadata.title}</title>
       </head>
       <body className={inter.className}>
-        {/*<Layout>{children}</Layout> {/* applying the custom layout */}
-        {children}
+        <Layout>{children}</Layout> {/* applying the custom layout */}
       </body>
     </html>
   );
