b7dd4c40011bc99957566fcc589ef0c3d7c28abd
Navin Chikkodi <73332645+og-nav@users.noreply.github.com>
2024-11-07 12:49:03 -0600

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { AppSidebar } from '@/components/app-sidebar';
-import { Canvas } from '@/components/canvas';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -42,7 +41,29 @@ export default function Page() {
           </div>
         </header>
         <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-          <Canvas />
+        <Button
+            onClick={async () => {
+              const config = {
+                function: 'x'
+              };
+              try {
+                const response = await fetch('http://localhost:8000/math', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(config),
+                });
+
+                const data = await response.json();
+                console.log('math:', data.result);
+              } catch (error) {
+                console.log('Sup Error:', error);
+              }
+            }}
+          >
+            Test Math Sonify
+          </Button>
           <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
             <div className='aspect-video rounded-xl bg-muted/50' />
             <div className='aspect-video rounded-xl bg-muted/50' />
